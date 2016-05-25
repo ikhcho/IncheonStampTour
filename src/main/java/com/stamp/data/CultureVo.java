57ea9080a209674a6d52f7b1bf69388be7f998a9
IkhyeonCho <sorhtfl12@gmail.com>
2016-05-25 21:24:12 +0900

    0525 add class

---
+++ src/main/java/com/stamp/data/CultureVo.java
@@ -0,0 +1,97 @@
+package com.stamp.data;
+
+public class CultureVo {
+	private String title;
+	private String link;
+	private String category;
+	private String sdate;
+	private String edate;
+	private String place;
+	private String fee_case;
+	private String fee;
+	private String tel;
+	private String homepage;
+	private String poster;
+	private String posterThumb;
+	private String description;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public String getEdate() {
+		return edate;
+	}
+	public void setEdate(String edate) {
+		this.edate = edate;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getFee_case() {
+		return fee_case;
+	}
+	public void setFee_case(String fee_case) {
+		this.fee_case = fee_case;
+	}
+	public String getFee() {
+		return fee;
+	}
+	public void setFee(String fee) {
+		this.fee = fee;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getHomepage() {
+		return homepage;
+	}
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getPosterThumb() {
+		return posterThumb;
+	}
+	public void setPosterThumb(String posterThumb) {
+		this.posterThumb = posterThumb;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ src/main/java/com/stamp/data/TreasureVo.java
@@ -0,0 +1,5 @@
+package com.stamp.data;
+
+public class TreasureVo {
+
+}
